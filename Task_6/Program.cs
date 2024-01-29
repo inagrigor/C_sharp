bbe8d05eb5a4ca798b06c2218a60478d85034bc5
Grigor_Ina <grigorina72@gmail.com>
2024-01-29 07:04:32 +0200

    tema de acasa Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 8;
+int b = 10;
+int product = a * b;
+Console.WriteLine(product);
+
+double c = 5;
+int d = 2;
+double sum = c + d;
+Console.WriteLine(sum);
+
+double quotient = product / sum;
+Console.WriteLine(quotient);
